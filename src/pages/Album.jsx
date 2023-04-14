0c93ed95ce0cdbd58e23e09bb48ccf72997c466f
alanguilhermeM <alanguilherme13@outlook.com>
2023-04-14 15:34:06 -0300

    Finalizando requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,52 @@
 import React, { Component } from 'react';
 import { Header } from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  state = {
+    musics: [],
+    name: '',
+    album: '',
+  };
+
+  async componentDidMount() {
+    const path = location.pathname;
+    const ids = path.split('/');
+    const idd = ids[2];
+    const result = await getMusics(idd);
+    const musics = result.map((music) => ({
+      musicName: music.trackName,
+      audio: music.previewUrl,
+      kind: music.kind,
+    }));
+    this.setState({
+      musics,
+      name: result[0].artistName,
+      album: result[0].collectionName,
+    });
+  }
+
   render() {
+    const { musics, name, album } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        <h3 data-testid="artist-name">{name}</h3>
+        <h4 data-testid="album-name">{album}</h4>
+        {musics.filter((music) => music.kind === 'song')
+          .map((music, index) => (
+            <div key={ index }>
+              <p data-testid="music-name">{ music.musicName }</p>
+              <audio data-testid="audio-component" src={ music.audio } controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                {' '}
+                {' '}
+                <code>audio</code>
+                .
+              </audio>
+            </div>
+          ))}
       </div>
     );
   }
