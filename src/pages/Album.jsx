08d58aeeab8ce9234b17a4ce6959190e9d1c6e8d
alanguilhermeM <alanguilherme13@outlook.com>
2023-04-17 09:19:46 -0300

    Trabalhando no requisito 8

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import { Header } from '../components/Header';
 import getMusics from '../services/musicsAPI';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class Album extends Component {
   state = {
     musics: [],
     name: '',
     album: '',
+    isLoading: false,
+    favoriteSongs: {},
   };
 
   async componentDidMount() {
-    const path = location.pathname;
-    const ids = path.split('/');
-    const idd = ids[2];
-    const result = await getMusics(idd);
+    const id = this.getId();
+    const result = await getMusics(id);
     const musics = result.map((music) => ({
       musicName: music.trackName,
       audio: music.previewUrl,
       kind: music.kind,
+      trackId: music.trackId,
     }));
     this.setState({
       musics,
@@ -26,27 +28,70 @@ class Album extends Component {
     });
   }
 
+  getId = () => {
+    const path = location.pathname;
+    const ids = path.split('/');
+    const idd = ids[2];
+    return idd;
+  };
+
+  handleFavorite = async (event) => {
+    const { favoriteSongs } = this.state;
+    const trackId = event.target.value;
+    const isFavorite = favoriteSongs[trackId] || false; // obtém o estado atual da música favorita
+    const updatedFavoriteSongs = {
+      ...favoriteSongs,
+      [trackId]: !isFavorite, // atualiza o estado da música favorita
+    };
+    const id = this.getId();
+    const result = await getMusics(id);
+    const musics = result.filter((music) => music.kind === 'song');
+    this.setState({
+      isLoading: true,
+      favoriteSongs: updatedFavoriteSongs,
+    });
+    await addSong(musics);
+    this.setState({
+      isLoading: false,
+    });
+  };
+
   render() {
-    const { musics, name, album } = this.state;
+    const { musics, name, album, isLoading, favoriteSongs } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        <h3 data-testid="artist-name">{name}</h3>
-        <h4 data-testid="album-name">{album}</h4>
-        {musics.filter((music) => music.kind === 'song')
-          .map((music, index) => (
-            <div key={ index }>
-              <p data-testid="music-name">{ music.musicName }</p>
-              <audio data-testid="audio-component" src={ music.audio } controls>
-                <track kind="captions" />
-                O seu navegador não suporta o elemento
-                {' '}
-                {' '}
-                <code>audio</code>
-                .
-              </audio>
+        {
+          isLoading ? <h1>Carregando...</h1> : (
+            <div>
+              <h3 data-testid="artist-name">{name}</h3>
+              <h4 data-testid="album-name">{album}</h4>
+              {musics.filter((music) => music.kind === 'song')
+                .map((music, index) => (
+                  <div key={ index }>
+                    <p data-testid="music-name">{ music.musicName }</p>
+                    <audio data-testid="audio-component" src={ music.audio } controls>
+                      <track kind="captions" />
+                      O seu navegador não suporta o elemento
+                      {' '}
+                      {' '}
+                      <code>audio</code>
+                      .
+                    </audio>
+                    <label data-testid={ `checkbox-music-${music.trackId}` }>
+                      Favorita
+                      <input
+                        type="checkbox"
+                        value={ music.trackId }
+                        onChange={ this.handleFavorite }
+                        checked={ favoriteSongs[music.trackId] || false }
+                      />
+                    </label>
+                  </div>
+                ))}
             </div>
-          ))}
+          )
+        }
       </div>
     );
   }
