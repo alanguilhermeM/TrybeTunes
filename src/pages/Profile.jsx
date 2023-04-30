6c63a1e4bfd0780167163ef9cc50c8ddf84fb010
alanguilhermeM <alanguilherme13@outlook.com>
2023-04-30 16:16:53 -0300

    FInalizando requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Header } from '../components/Header';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    isLoading: true,
+    profile: [],
+  };
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({
+      isLoading: false,
+      profile: [user],
+    });
+  }
+
   render() {
+    const { isLoading, profile } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {isLoading ? <h2>Carregando...</h2> : (
+          profile.map((user) => (
+            <div key={ Math.random() }>
+              <h3>{ user.name }</h3>
+              <h4>{ user.email }</h4>
+              <h4>{ user.description }</h4>
+              <img data-testid="profile-image" src={ user.image } alt="profile img" />
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+          ))
+        )}
       </div>
     );
   }
